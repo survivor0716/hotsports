fffa22ed4254559fac02890330bba8c50870b76f
Survivor <dongchen1022@gmail.com>
2016-02-28 23:46:44 +0800

    关于compile和link 一个复杂一点的例子Expander

--- app/scripts/directives/myDirective.js
+++ app/scripts/directives/myDirective.js
@@ -1,9 +1,25 @@
 angular.module('hotsportsApp')
-  .directive('hello', function() {
-  return {
-    restrict: 'E',
-    template: '<div>Hi there <span ng-transclude></span></div>',
-    replace: true,
-    transclude: true
-  };
-});
+  .directive('expander', function () {
+    return {
+      restrict  : 'EA',
+      replace   : true,
+      transclude: true,
+      scope     : {
+        title: '=expanderTitle'
+      },
+      template  : '<div>'
+      + '<div class="title" ng-click="toggle()">{{title}}</div>'
+      + '<div class="body" ng-show="showMe" ng-transclude></div>'
+      + '</div>',
+      link      : function (scope, element, attrs) {
+        scope.showMe = false;
+        scope.toggle = function toggle() {
+          scope.showMe = !scope.showMe;
+        }
+      }
+    }
+  })
+  .controller('SomeController', function ($scope) {
+    $scope.title = '点击展开';
+    $scope.text = '这里是内部的内容。';
+  });
