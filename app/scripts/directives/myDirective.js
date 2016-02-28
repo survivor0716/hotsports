5eaa1a893a13d84534e77eaf1a46702032ac0d53
Survivor <dongchen1022@gmail.com>
2016-02-29 00:28:39 +0800

    scope取值不同的差别:
    当为false时候，儿子继承父亲的值，改变父亲的值，儿子的值也随之变化，反之亦如此。（继承不隔离）
    
    当为true时候，儿子继承父亲的值，改变父亲的值，儿子的值随之变化，但是改变儿子的值，父亲的值不变。（继承隔离）
    
    当为{}时候，没有继承父亲的值，所以儿子的值为空，改变任何一方的值均不能影响另一方的值。（不继承隔离）

--- app/scripts/directives/myDirective.js
+++ app/scripts/directives/myDirective.js
@@ -1,25 +1,11 @@
 angular.module('hotsportsApp')
-  .directive('expander', function () {
-    return {
-      restrict  : 'EA',
-      replace   : true,
-      transclude: true,
-      scope     : {
-        title: '=expanderTitle'
-      },
-      template  : '<div>'
-      + '<div class="title" ng-click="toggle()">{{title}}</div>'
-      + '<div class="body" ng-show="showMe" ng-transclude></div>'
-      + '</div>',
-      link      : function (scope, element, attrs) {
-        scope.showMe = false;
-        scope.toggle = function toggle() {
-          scope.showMe = !scope.showMe;
-        }
-      }
-    }
+  .controller('TestScopeController', function ($scope) {
+    $scope.name = 'leifeng';
   })
-  .controller('SomeController', function ($scope) {
-    $scope.title = '点击展开';
-    $scope.text = '这里是内部的内容。';
+  .directive('myDirective', function () {
+    return {
+      restrict: 'EA',
+      scope: true,//改变此处的取值,看看有什么不同
+      template: '<div>儿子:{{ name }}<input ng-model="name"/></div>'
+    };
   });
